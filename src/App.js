bb0022d4d88f87c69b520e866f3b3dabe20d462e
Yutan0423 <yuutan3240@gmail.com>
2021-04-25 17:45:27 +0900

    permit audio and video

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
-function App() {
+
+
+const getMedia = async () => {
+  const constraints = {
+    audio: true,
+    video: true,
+  }
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+    /* ストリームを使用 */
+  } catch(err) {
+    /* エラーを処理 */
+  }
+}
+
+getMedia();
+
+const App = () => {
   return <div>Hello, React!</div>;
 }
 
