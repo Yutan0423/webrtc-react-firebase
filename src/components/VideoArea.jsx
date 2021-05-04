2c42b06206c98ce23198742fce1ae13922fb30b1
Yutan0423 <yuutan3240@gmail.com>
2021-05-04 16:44:03 +0900

    refine video

--- src/components/VideoArea.jsx
+++ src/components/VideoArea.jsx
@@ -19,10 +19,11 @@ const VideoArea = ({ rtcClient }) => {
   const classes = useStyles();
 
   if(rtcClient === null) return <></>;
+  if(rtcClient.localPeerName === '' || rtcClient.remotePeerName === '') return <></>;
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
+      <Grid container spacing={0}>
         <Grid item xs={12} sm={6}>
           <VideoLocal rtcClient={rtcClient} />
         </Grid>
